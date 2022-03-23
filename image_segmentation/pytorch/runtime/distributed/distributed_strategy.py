336d30efeefcde2ed586e775221bfb6476c57c34
Albert Liang <albertliang@google.com>
2022-03-23 15:15:38 -0400

    Minor code style improvement

--- image_segmentation/pytorch/runtime/distributed/distributed_strategy.py
+++ image_segmentation/pytorch/runtime/distributed/distributed_strategy.py
@@ -12,8 +12,7 @@ class DistributedStrategy(ABC):
 
     @abstractmethod
     def get_rank(self) -> int:
-        """
-        Gets distributed rank or returns 0 if distributed is not initialized
+        """Gets distributed rank or returns 0 if distributed is not initialized
 
         :return: the distributed rank
         :rtype: int
@@ -22,8 +21,7 @@ class DistributedStrategy(ABC):
 
     @abstractmethod
     def get_world_size(self) -> int:
-        """
-        Gets distributed world size or returns 1 if distributed is not initialized
+        """Gets distributed world size or returns 1 if distributed is not initialized
 
         :return: the distributed world size
         :rtype: int
@@ -32,8 +30,7 @@ class DistributedStrategy(ABC):
 
     @abstractmethod
     def get_device(self, local_rank: int) -> torch.device:
-        """
-        Sets and gets the backend device associated with the local rank
+        """Sets and gets the backend device associated with the local rank
 
         :param int local_rank: the local rank for the backend device
         :return: the backend device
@@ -43,15 +40,12 @@ class DistributedStrategy(ABC):
 
     @abstractmethod
     def barrier(self):
-        """
-        Distributed barrier to synchronize all processes
-        """
+        """Distributed barrier to synchronize all processes"""
         pass
 
     @abstractmethod
     def seed_everything(self, seed: int):
-        """
-        Seeds random state for backend devices
+        """Seeds random state for backend devices
 
         :param int seed: the random seed to set
         """
@@ -59,8 +53,7 @@ class DistributedStrategy(ABC):
 
     @abstractmethod
     def broadcast_seeds(self, seeds: List[int], device: torch.device) -> List[int]:
-        """
-        Broadcasts the random seeds from the master to all distributed workers
+        """Broadcasts the random seeds from the master to all distributed workers
 
         :param List[int] seeds: list of seeds to broadcast from the master
         :param torch.device device: the backend device
@@ -71,8 +64,7 @@ class DistributedStrategy(ABC):
 
     @abstractmethod
     def reduce_tensor(self, tensor: torch.Tensor) -> torch.Tensor:
-        """
-        All-reduces the tensor from all workers using sum operation
+        """All-reduces the tensor from all workers using sum operation
 
         :param torch.Tensor tensor: the tensor to all reduce over
         :return: the all-reduced tensor
@@ -81,8 +73,7 @@ class DistributedStrategy(ABC):
         pass
 
     def is_main_process(self) -> bool:
-        """
-        Returns trues if it is the master process
+        """Returns trues if it is the master process
 
         :return: true if it is the master process, false otherwise
         :rtype: bool
