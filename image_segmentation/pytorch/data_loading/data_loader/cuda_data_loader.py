2c9073d74ea038ecfdfa36b0736ae0113f443483
Albert Liang <albertliang@google.com>
2022-03-23 15:19:18 -0400

    Minor code style improvement

--- image_segmentation/pytorch/data_loading/data_loader/cuda_data_loader.py
+++ image_segmentation/pytorch/data_loading/data_loader/cuda_data_loader.py
@@ -11,8 +11,7 @@ def get_data_loaders(
     train_dataset: Dataset,
     val_dataset: Dataset,
 ) -> Tuple[DataLoader, DataLoader]:
-    """
-    Initializes and returns (train_data_loader, val_data_loader) as Dataloader objects
+    """Initializes and returns (train_data_loader, val_data_loader) as Dataloader objects
 
     :param Namespace flags: the runtime arguments
     :param int num_shards: number of shards for the train dataset
