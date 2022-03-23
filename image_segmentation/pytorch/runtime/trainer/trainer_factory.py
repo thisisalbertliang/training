e080238c47df6524338f7d520c931f55f113ee53
Albert Liang <albertliang@google.com>
2022-03-22 22:12:57 -0400

    Replace flags.torch_xla with flags.device for better extensibility to other backends in the future

--- image_segmentation/pytorch/runtime/trainer/trainer_factory.py
+++ image_segmentation/pytorch/runtime/trainer/trainer_factory.py
@@ -4,12 +4,14 @@ from runtime.trainer.xla_trainer import XLATrainer
 
 def get_trainer(flags, model, train_loader, val_loader, loss_fn, score_fn, device, callbacks):
     """Initializes and return the trainer (XLATrainer or CUDATrainer)"""
-    if flags.torch_xla:
+    if flags.device == "xla":
         trainer = XLATrainer(
             flags, model, train_loader, val_loader, loss_fn, score_fn, device, callbacks
         )
-    else:
+    elif flags.device == "cuda":
         trainer = CUDATrainer(
             flags, model, train_loader, val_loader, loss_fn, score_fn, device, callbacks
         )
+    else:
+        raise ValueError(f"Device {flags.device} unknown. Valid devices are: cuda, xla")
     return trainer
