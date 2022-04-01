827a2dcce3c690cde97e5b0349aafed163642a92
Albert Liang <albertliang@google.com>
2022-03-31 21:42:08 -0400

    Add interactive mode to capture_profile.py

--- image_segmentation/pytorch/capture_profile.py
+++ image_segmentation/pytorch/capture_profile.py
@@ -29,14 +29,43 @@ def main():
         default=30000,
         help="duration in milliseconds for tracing the server.",
     )
+    parser.add_argument(
+        "--interactive",
+        dest="interactive",
+        type=str,
+        choices=[None, "once", "loop"],
+        default=None,
+        help=(
+            "run in interactive mode.\n"
+            'If set to "once", the profiler client asks for user confirmation before starting profiling.\n'
+            'If set to "loop", the profiler client repeatedly runs profiling, asking for user confirmation on each run.\n'
+            "Defaults to None, which disables interactive mode."
+        ),
+    )
 
     flags = parser.parse_args()
 
-    xp.trace(
-        service_addr=flags.service_addr,
-        logdir=flags.logdir,
-        duration_ms=flags.duration_ms,
-    )
+    def trace():
+        xp.trace(
+            service_addr=flags.service_addr,
+            logdir=flags.logdir,
+            duration_ms=flags.duration_ms,
+        )
+        print(f"Saved profiling output to {flags.logdir}")
+
+    def request_user_confirmation():
+        input("Press enter to start profiling:")
+
+    # Run performance profiling
+    if flags.interactive == "once":
+        request_user_confirmation()
+        trace()
+    elif flags.interactive == "loop":
+        while True:
+            request_user_confirmation()
+            trace()
+    else:
+        trace()
 
 
 if __name__ == "__main__":
